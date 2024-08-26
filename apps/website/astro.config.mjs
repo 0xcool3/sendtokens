d4e8005e34fd33b46e6aa55dc6f4689e34b0bf16
000luna000 <000luna000@proton.me>
2024-08-26 17:00:43 +0800

    Update domain

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -21,7 +21,7 @@ const SERVER_PORT = 4400;
 // the url to access your blog during local development
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 // the url to access your blog after deploying it somewhere (Eg. Netlify)
-const LIVE_URL = "https://sendtokens.cool3.io";
+const LIVE_URL = "https://app.sendtokens.cool3.io";
 // this is the astro command your npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
